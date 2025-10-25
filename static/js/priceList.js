2c2e9d37cc1336cff86ee7e0a260e9a7f5194999
Vitny <thevitny@gmail.com>
2025-10-25 23:37:52 +0300

    feat: update price-list new design, styles

--- static/js/priceList.js
+++ static/js/priceList.js
@@ -1,18 +1,19 @@
-// price list
-document.addEventListener("DOMContentLoaded", () => {
-  const adultBtn = document.querySelector('.adult-price-p');
-  const kidsBtn = document.querySelector('.kids-price-p');
-  const activeBg = document.querySelector('.toggle-active-bg');
+// по переключателю должен менять список услуг/цен
+// ниже реализован только визуальный функционал асамого перекл.чателя, списорк сейчас не меняется
+document.addEventListener('DOMContentLoaded', () => {
+  const adultBtn = document.querySelector('.adult-price-p')
+  const kidsBtn = document.querySelector('.kids-price-p')
+  const activeBg = document.querySelector('.toggle-active-bg')
 
   adultBtn.addEventListener('click', () => {
-    activeBg.style.top = '0';
-    adultBtn.classList.add('active-btn');
-    kidsBtn.classList.remove('active-btn');
-  });
+    activeBg.style.top = '0'
+    adultBtn.classList.add('active-btn')
+    kidsBtn.classList.remove('active-btn')
+  })
 
   kidsBtn.addEventListener('click', () => {
-    activeBg.style.top = '50%';
-    kidsBtn.classList.add('active-btn');
-    adultBtn.classList.remove('active-btn');
-  });
-});
+    activeBg.style.top = '50%'
+    kidsBtn.classList.add('active-btn')
+    adultBtn.classList.remove('active-btn')
+  })
+})
